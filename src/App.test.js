52b1bfd8bf7af460346ceeb38e046d926ef1084d
andrieucyril31 <andrieucyril31@gmail.com>
2020-04-12 22:00:18 +0200

    add test ko

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link KO', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react KOOOOO/i);
+    expect(linkElement).toBeInTheDocument();
+});
